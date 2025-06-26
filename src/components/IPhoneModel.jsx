893d394876c8c9c6f021fb38089206fc61993f22
Kunal Shinde <skunal148@gmail.com>
2025-06-26 12:04:21 +0530

    mobile_success

--- src/components/IPhoneModel.jsx
+++ src/components/IPhoneModel.jsx
@@ -27,12 +27,18 @@ const IPhoneModel = ({ videoSrc }) => {
     }
 
     const texture = new THREE.VideoTexture(video);
+    texture.colorSpace = THREE.SRGBColorSpace; // <--- Add this line
+    texture.generateMipmaps = false;
+    texture.minFilter = THREE.LinearFilter;
+    texture.magFilter = THREE.LinearFilter;
+    texture.wrapS = THREE.ClampToEdgeWrapping;
+    texture.wrapT = THREE.ClampToEdgeWrapping;
     texture.repeat.set(1, 1);   // Controls zoom/stretch
     texture.offset.set(0, 0); // Controls position shift
-    
-    
 
-video.onplay = () => console.log("🎥 Video started playing");
+
+
+    video.onplay = () => console.log("🎥 Video started playing");
 
 
     scene.traverse((child) => {
@@ -42,18 +48,14 @@ video.onplay = () => console.log("🎥 Video started playing");
           child.material = child.material.clone();
           child.material.metalness = 1;
           child.material.roughness = 0.9;
-          
-        }
- 
-        if(child.name === 'ttmRoLdJipiIOmf')
-        {
-          child.visible = false;
+
         }
 
-        
+
+
         if (child.name === 'xXDHkMplTIDAXLN') {
           console.log('✅ Applying video texture to mesh:', child.name);
-          
+
           child.material = new THREE.MeshBasicMaterial({
             map: texture,
             side: THREE.DoubleSide,
@@ -68,7 +70,7 @@ video.onplay = () => console.log("🎥 Video started playing");
     <group
       scale={27}
       position={[-0.2, 0.2, 0.1]}
-      rotation={[-0.2, Math.PI-0.4, 0.1]}
+      rotation={[-0.2, Math.PI - 0.4, 0.1]}
     >
       <primitive object={scene} />
     </group>
